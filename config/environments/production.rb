867f5704d2fa60690c42d95f10dede7778f0d5dc
klyde <klydem@yahoo.co.uk>
2022-04-03 19:20:54 +0100

    FIX:0001: Fix production db:migrate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -134,5 +134,5 @@ Rails.application.configure do
     # not stored in the Git repository, to access those secrets.
     # Enable this option inside config/environments/production.rb:
     config.require_master_key = true
-    config.web_console.whiny_requests = false
+    # config.web_console.whiny_requests = false
 end
