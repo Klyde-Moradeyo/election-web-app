5c35a9fd639c87d8eb584710a1791954c2fcb05f
klyde <klydem@yahoo.co.uk>
2022-04-08 16:10:42 +0100

    FEAT000: Add ballot validation and relationships

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,8 +1,13 @@
 class Ballot < ApplicationRecord
-  validates_presence_of :user, :title, :url
+  validates_presence_of :user, :title, :URL, :ballot_type, :start_date, :end_date
 
   # ==========================
   # RELATIONSHIPS
   # ==========================
   belongs_to :user
+  has_many :questions
+  has_many :partys
+  has_many :options, through: :questions
+  has_many :ballot_results
+  has_many :question_results, through: :questions
 end
