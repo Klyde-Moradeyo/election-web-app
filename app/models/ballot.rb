024cb293c6edcdd5406a1cb9cc101d028571b14a
klyde <klydem@yahoo.co.uk>
2022-04-08 15:31:06 +0100

    FEAT0000: Add Ballot validation

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,3 +1,8 @@
 class Ballot < ApplicationRecord
+  validates_presence_of :user, :title, :url
+
+  # ==========================
+  # RELATIONSHIPS
+  # ==========================
   belongs_to :user
 end
