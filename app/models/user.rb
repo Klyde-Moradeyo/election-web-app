d0e59604ce8c95c006e54792d92191ae6a22badd
klyde <klydem@yahoo.co.uk>
2022-04-13 16:00:05 +0100

    FEAT0002: Modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     has_many :voters, dependent: :destroy
     has_many :stored_voters, dependent: :destroy
     has_many :ballots, dependent: :destroy
-    has_one :permissions, dependent: :destroy
+    # has_one :permissions, dependent: :destroy
 
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
