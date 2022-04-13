fbdad9ba9397ddee798e1ee2e452778864ce6c28
klyde <klydem@yahoo.co.uk>
2022-04-13 15:11:43 +0100

    FEAT0001: Fix home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
-    render layout: false
+    before_action :render
     def index
         if user_signed_in?
             redirect_to ballots_path
         end
+    end
+
+    def render
+        render layout: false
     end
 
 end
