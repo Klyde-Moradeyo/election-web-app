a869eaa6d55f9faee0c989f7f286733ee4361bba
klyde <klydem@yahoo.co.uk>
2022-04-13 21:27:17 +0100

    FEAT0002: Modify routes.rb to be nested

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,22 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  # resources
-  resources :users
-  resources :voters
-  resources :stored_voters
-  resources :permissions
-  resources :ballots
-  resources :parties
-  resources :ballot_results
-  resources :questions
-  resources :question_results
-  resources :options
-  resources :home
-
   # Defines the root path route ("/")
   root "home#index"
+
+  # resources
+  resources :home
+  resources :users do
+    resources :stored_voters
+    # resources :stored_parties
+    resources :ballots do
+      resources :parties
+      resources :voters
+      resources :ballot_results
+      resources :questions do
+        resources :options
+        resources :question_results
+      end
+    end
+  end
 end
