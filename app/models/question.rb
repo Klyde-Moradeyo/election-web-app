fd83c6c951b890703945960e8102449a7ae0fa80
klyde <klydem@yahoo.co.uk>
2022-04-18 13:32:25 +0100

    FEAT002: Fix destroy ballot

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,6 @@ class Question < ApplicationRecord
   # RELATIONSHIPS
   # ==========================
   belongs_to :ballot
-  has_many :options
+  has_many :options, dependent: :destroy
   accepts_nested_attributes_for :options, reject_if: proc { |attributes| attributes['title'].blank? }
 end
