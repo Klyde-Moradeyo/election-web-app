9e795c0cd39942c6fbb7f123b241ec54108c06cf
klyde <klydem@yahoo.co.uk>
2022-04-23 16:45:49 +0100

    FEAT0009: Rubocop autocorrect

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   # ==========================
   has_many :stored_voters, dependent: :destroy
   has_many :ballots, dependent: :destroy, through: :roles, source: :resource, source_type: :Ballot
-  
+
   after_create :assign_default_role
   validate :must_have_a_role, on: :update
 
