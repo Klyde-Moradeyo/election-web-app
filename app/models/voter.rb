4625616ae952f1d1df18e82759379c64289fccae
klyde <klydem@yahoo.co.uk>
2022-04-23 18:15:57 +0100

    FEAT0011: Remove voter scaffold

--- app/models/voter.rb
+++
@@ -1,8 +0,0 @@
-class Voter < ApplicationRecord
-  validates :username, :password, presence: true
-
-  # ==========================
-  # RELATIONSHIPS
-  # ==========================
-  belongs_to :ballot
-end
