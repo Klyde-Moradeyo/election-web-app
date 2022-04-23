7c6713a625ad01aa43e167c37539d4d1290783de
klyde <klydem@yahoo.co.uk>
2022-04-23 18:15:29 +0100

    FEAT0011: Remove stored_voter scaffold

--- app/models/stored_voter.rb
+++
@@ -1,8 +0,0 @@
-class StoredVoter < ApplicationRecord
-  validates :username, :password, :vote_weight, presence: true
-
-  # ==========================
-  # RELATIONSHIPS
-  # ==========================
-  belongs_to :user
-end
--- app/views/stored_voters/_stored_voter.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! stored_voter, :id, :user_id, :username, :password, :email, :vote_weight, :created_at, :updated_at
-json.url stored_voter_url(stored_voter, format: :json)
