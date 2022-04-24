de92dfcc55b01bdbbf29c207b4aff70d28b76365
klyde <klydem@yahoo.co.uk>
2022-04-24 22:10:28 +0100

    REFACTOR0056: Modify build scripts

--- bin/git_merge_and_push.sh
+++ bin/git_merge_and_push.sh
@@ -4,12 +4,16 @@ git stash
 git checkout -b $1
 git stash pop;
 
-# Remove build files before commit
-[ ! -e VERSION ] || rm -rf VERSION
-[ ! -e .jx/variables.sh ] || rm -rf .jx/variables.sh
+# Move build files before commit
+[ ! -e VERSION ] || mv VERSION .. 
+[ ! -e .jx/variables.sh ] || mv .jx/variables.sh ..
 
 git add .
 git commit -m "$2"
 git checkout $3
 git merge $1
 git checkout --force $3
+
+# Move build files after commit
+[ ! -e ../VERSION ] || mv ../VERSION . 
+[ ! -e ../variables.sh ] || mv ../variables.sh ./.jx
