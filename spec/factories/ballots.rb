5e65e8d8134f47009b5374ab40f98b9f9b4ebe80
klyde <klydem@yahoo.co.uk>
2022-04-24 17:23:07 +0100

    TEST0011: Modify ballot test after migration

--- spec/factories/ballots.rb
+++ spec/factories/ballots.rb
@@ -3,11 +3,9 @@ FactoryBot.define do
     user # Associaiton
     sequence(:title) { |n| "LECTURE_#{n}" }  
     description{ "This a ballot." }  
-    URL{ "Generated URL form ballot" }  
-    ballot_type{ "Poll" }  
     start_date{ DateTime.now  }  
     end_date{ DateTime.now + 1.week }  
-    weighted_voting{ 1 }  
     show_results { 1 }  
+    access_token { 12111121 }
   end
 end
