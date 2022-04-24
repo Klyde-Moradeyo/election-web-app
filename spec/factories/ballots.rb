7148c63a5ac589396946fc3a6586350df903c8ab
klyde <klydem@yahoo.co.uk>
2022-04-24 20:26:41 +0100

    TEST0011: Modify ballots.rb (M)

--- spec/factories/ballots.rb
+++ spec/factories/ballots.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     description{ "This a ballot." }  
     start_date{ DateTime.now  }  
     end_date{ DateTime.now + 1.week }  
-    show_results { 1 }  
+    show_voters_results { 1 }  
   end
 end
