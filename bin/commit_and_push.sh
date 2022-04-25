b44a0ec9cc6e51707eb38769bfd158990d252f61
klyde <klydem@yahoo.co.uk>
2022-04-25 12:12:08 +0100

    REFACTOR0058: Modify build scripts and release.yaml

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -4,15 +4,15 @@ git stash
 git checkout --force $1 
 git stash pop;
 
-# Move build files before commit
-[ ! -e VERSION ] || mv -v VERSION .. 
-[ ! -e .jx/variables.sh ] || mv -v .jx/variables.sh ..
+# Remove build files before commit
+[ ! -e VERSION ] || rm -rf VERSION
+[ ! -e .jx/variables.sh ] || rm -rf .jx/variables.sh
 
 git add .
 git commit -m "$2"
 git push origin
-git checkout --force main
+git checkout --force $3
 
 # Move build files after commit
 [ ! -e ../VERSION ] || mv -v ../VERSION . 
-[ ! -e ../variables.sh ] || mkdir ./.jx || mv -v ../variables.sh ./.jx/variables.sh 
+[ ! -e ../variables.sh ] || mkdir -v --parents ./.jx; mv -v ../variables.sh $_
