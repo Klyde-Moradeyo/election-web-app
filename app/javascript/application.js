d6558515775b1511a6a6753e0f93a0c0708fdf31
klyde <klydem@yahoo.co.uk>
2022-05-05 12:52:19 +0100

    REFACTOR0064: Fix rails assets pipeline

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require rails-ujs
 require("bootstrap");
+require("./main");
+require("../assets/stylesheets/main.css");
 import * as bootstrap from "bootstrap"
 
