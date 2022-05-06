663b766ceee03e070693ef633fc43e793e21cc8c
klyde <klydem@yahoo.co.uk>
2022-05-06 12:54:41 +0100

    REFACTOR0065: Add admin to db see

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+role = Role.find_or_create_by(name: 'admin') # create or find admin role
+# creating or finding first admin user
+User.create_or_find_by(first_name: "admin", last_name: "user", email: "admin@esa.com",
+                       password: "ESA_admin_user", password_confirmation: 'ESA_admin_user', roles: [role],
+                       confirmed_at: DateTime.now)
