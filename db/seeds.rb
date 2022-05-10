a2cb82b240e3a5cfd97c60b0be06e022ddc598d0
klyde <klydem@yahoo.co.uk>
2022-05-10 18:26:49 +0100

    TEST0014: Create database seeds and add it to spec_helper

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# creating or finding first admin User
 role = Role.find_or_create_by(name: 'admin') # create or find admin role
-# creating or finding first admin user
-User.create_or_find_by(first_name: "admin", last_name: "user", email: "admin@esa.com",
-                       password: "ESA_admin_user", password_confirmation: 'ESA_admin_user', roles: [role],
-                       confirmed_at: DateTime.now)
+admin = User.create_or_find_by( first_name: "admin", last_name: "Moradeyo", email: "admin@klydemoradeyo.com",
+                                password: "12345678", password_confirmation: '12345678', roles: [role],
+                                confirmed_at: DateTime.now)
+
+# Creating or finding User                         
+user_role = Role.find_or_create_by(name: 'newuser')
+user = User.create_or_find_by(  first_name: "Klyde", last_name: "Moradeyo", email: "km00905@surrey.ac.uk",
+                                password: "12345678", password_confirmation: '12345678', roles: [user_role],
+                                confirmed_at: DateTime.now)
+
+ballot = Ballot.create_or_find_by(title: "title", description: "desc", start_date: DateTime.now,
+                                  end_date: DateTime.now + 3.days, show_voter_results: true,
+                                  expected_voters: 50, voting_type: "Popular vote")
