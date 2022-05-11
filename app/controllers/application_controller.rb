8ca5055c0e7a6c21e3745e9db9d310faecf0d2ec
klydem11 <50842133+klydem11@users.noreply.github.com>
2022-05-11 13:41:29 -0700

    FIX0002: Fix letter_opener on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # These 2 lines should be removed once the applications goes live
+  before_action :set_default_url_options
+  skip_before_action :verify_authenticity_token
+
+  def set_default_url_options
+    ActionMailer::Base.default_url_options = { host: request.host_with_port }
+  end
 end
