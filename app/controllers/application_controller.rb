d9b6755e9828bb3b9142454cf91c61956b6a10fa
klyde <klydem@yahoo.co.uk>
2022-05-11 17:19:02 +0100

    FIX0002: Modify application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  before_action :set_default_url_options
+
+  protected
+  def set_default_url_options
+    ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  end
 end
