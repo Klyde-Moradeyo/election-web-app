6fd1fcd63c73040063f798f6baa9e3bae8ee24f0
klyde <klydem@yahoo.co.uk>
2022-05-11 18:31:14 +0100

    FIX0002: Modfiy applicaiton_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   # These 2 lines should be removed once the applications goes live
-  before_filter :set_default_url_options
+  before_action :set_default_url_options
   skip_before_action :verify_authenticity_token
 
   def set_default_url_options
