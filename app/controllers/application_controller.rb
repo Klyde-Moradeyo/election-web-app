33beed514e73a67d0023f2240407f6e37b432591
klyde <klydem@yahoo.co.uk>
2022-05-11 17:38:58 +0100

    FIX0002: Modify application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   # These 2 lines should be removed once the applications goes live
-  before_action :set_default_url_options
+  before_filter :set_default_url_options
   skip_before_action :verify_authenticity_token
-
-  protected
 
   def set_default_url_options
     ActionMailer::Base.default_url_options = { host: request.host_with_port }
