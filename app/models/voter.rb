e67214bf29d693fe6c43ea2dea5aaad4bad9c11d
klyde <klydem@yahoo.co.uk>
2022-05-16 01:29:01 +0100

    REFACTOR0077: Add null check and validation into voter (M)

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,5 +1,6 @@
 class Voter < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  validates :email, :password, presence: true
 
   # ==========================
   # RELATIONSHIPS
--- db/migrate/20220512050633_add_fields_in_voter.rb
+++ db/migrate/20220512050633_add_fields_in_voter.rb
@@ -1,6 +1,6 @@
 class AddFieldsInVoter < ActiveRecord::Migration[7.0]
   def change
-    add_column :voters, :email, :string
-    add_column :voters, :password, :string
+    add_column :voters, :email, :string, null: false
+    add_column :voters, :password, :string, null: false
   end
 end
