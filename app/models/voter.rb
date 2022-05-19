f125633eb977b6b345f1c0891614903dd0d468ca
klyde <klydem@yahoo.co.uk>
2022-05-19 14:29:12 +0100

    FEAT0031: Add new validations to voter

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,7 @@
 class Voter < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :email, :password, presence: true
+  validates :username, presence: true, uniqueness: true, uniqueness: { scope: [:ballot ] }
+  validates :email, presence: true, length: { maximum: 100 }
+  validate :email_format
 
   # ==========================
   # RELATIONSHIPS
@@ -8,4 +9,9 @@ class Voter < ApplicationRecord
   belongs_to :ballot
 
   has_many :question_results, dependent: :destroy
+
+  private
+  def email_format
+    validates_format_of :email, :with => Devise::email_regexp if email.present?
+  end
 end
