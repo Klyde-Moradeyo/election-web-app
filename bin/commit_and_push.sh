64877740aa74bf5b2006b9d040c8382b7b242ebf
klyde <klydem@yahoo.co.uk>
2022-05-22 23:27:01 +0100

    REFACTOR0084: Modify commit_and_push.sh

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -10,7 +10,7 @@ git stash pop;
 [ ! -e VERSION ] || mv -v VERSION .. 
 [ ! -e .jx/variables.sh ] || mv -v .jx/variables.sh ..
 
-git add .
+git add $4
 git commit -m "$2"
 git push origin
 git checkout --force $3
