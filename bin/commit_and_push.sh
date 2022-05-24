7bd34bee68b370e73ed38993b250a652facc21bd
klyde <klydem@yahoo.co.uk>
2022-05-24 11:41:38 +0100

    REFACTOR0084: Modify commit_and_push

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -3,18 +3,10 @@ git fetch --all
 git stash
 git checkout --force $1 
 
-[ ! -e public/coverage ] || rm -rf public/coverage # Delete public/coverage if exist
+rm -rf public/coverage # Delete public/coverage if exist
 git stash pop;
-
-# Move build files before commit
-[ ! -e VERSION ] || mv -v VERSION .. 
-[ ! -e .jx/variables.sh ] || mv -v .jx/variables.sh ..
 
 git add .
 git commit -m "$2"
 git push origin
 git checkout --force $3
-
-# Move build files after commit
-[ ! -e ../VERSION ] || mv -v ../VERSION . 
-[ ! -e ../variables.sh ] || mkdir -v --parents ./.jx; mv -v ../variables.sh ./.jx/variables.sh
