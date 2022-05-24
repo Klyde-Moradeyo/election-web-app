40fd5bac919ad280a94f969bb90edeb118e001ae
klyde <klydem@yahoo.co.uk>
2022-05-24 12:59:52 +0100

    REFACTOR0084: Modifygit_merge_and_push.sh

--- bin/git_merge_and_push.sh
+++ bin/git_merge_and_push.sh
@@ -4,17 +4,9 @@ git stash
 git checkout -b $1
 git stash pop;
 
-# Move build files before commit
-[ ! -e VERSION ] || mv -v VERSION .. 
-[ ! -e .jx/variables.sh ] || mv -v .jx/variables.sh ..
-
 git add $4
 git commit -m "$2"
 git checkout $3
 git merge $1
 git checkout --force $3
 git push origin
-
-# Move build files after commit
-[ ! -e ../VERSION ] || mv -v ../VERSION . 
-[ ! -e ../variables.sh ] || mkdir -v --parents ./.jx; mv -v ../variables.sh ./.jx/variables.sh
