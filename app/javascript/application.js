2e0b262d45cf04dac41fac5f59e0f85164769ba2
klyde <klydem@yahoo.co.uk>
2022-05-26 21:18:00 +0100

    FEAT0049: Add jquery-ujs to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require rails-ujs
 require("jquery");
+
 require("@nathanvda/cocoon");
 require("bootstrap");
 require("./main");
 require("../assets/stylesheets/main.css");
 import Chart from "chart.js/auto";
 import * as bootstrap from "bootstrap";
+import {} from 'jquery-ujs';
