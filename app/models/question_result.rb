ee0748a96d8dad50ed8a552abaa25fb3bdfc1a4b
klyde <klydem@yahoo.co.uk>
2022-05-26 16:06:38 +0100

    FEAT0043: Modify question_result to include ranks (MC)

--- app/models/question_result.rb
+++ app/models/question_result.rb
@@ -1,5 +1,6 @@
 class QuestionResult < ApplicationRecord
   validates :question, uniqueness: { scope: [:ballot, :voter] }
+  scope :voter_result, ->(voter_id){ where(voter_id: voter_id) }
 
   # ==========================
   # RELATIONSHIPS
@@ -8,4 +9,7 @@ class QuestionResult < ApplicationRecord
   belongs_to :ballot
   belongs_to :voter
   belongs_to :option
+
+  has_many :question_result_ranks, dependent: :destroy
+  accepts_nested_attributes_for :question_result_ranks, allow_destroy: true
 end
