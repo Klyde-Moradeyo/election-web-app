6b90209a41c232025f51271b85cb275a6b3524e5
klyde <klydem@yahoo.co.uk>
2022-05-30 09:25:33 +0100

    Revert "FEAT0065: Generate Voter Mailer"
    This reverts commit ec848d5032c98f84d10bfc07670fe9ad1e5f0dd9.

--- app/mailers/voter_mailer.rb
+++
@@ -1,2 +0,0 @@
-class VoterMailer < ApplicationMailer
-end
--- spec/mailers/previews/voter_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/voter_mailer
-class VoterMailerPreview < ActionMailer::Preview
-
-end
